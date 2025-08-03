835fab0828c580118a917a0ecf54dc40685e490b
Dmytro Teren <leo20091986@gmail.com>
2025-08-03 18:45:48 +0300

    add express server

---
+++ src/utils/getEnvVar.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+export function getEnvVar(name, defaultVal) {
+  const val = process.env[name];
+
+  if (val) return val;
+  if (defaultVal) return defaultVal;
+
+  throw new Error(`Missing: process.env[${name}]`);
+}
